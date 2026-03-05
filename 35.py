b46a7627cf88cdefad41cc3ef021863e2d581d35
nawras <graoui.nawras@etu.uae.ac.ma>
2026-03-05 11:11:23 +0000

    Add files via upload

---
+++ 35.py
@@ -0,0 +1,19 @@
+class Vehicule:
+    def demarrer(self):
+        print("Vehicule")
+
+
+class Voiture(Vehicule):
+    def demarrer(self):
+        print("Voiture")
+
+
+class Moto(Vehicule):
+    def demarrer(self):
+        print("Moto")
+
+
+vehicules = [Voiture(), Moto()]
+
+for v in vehicules:
+    v.demarrer()
